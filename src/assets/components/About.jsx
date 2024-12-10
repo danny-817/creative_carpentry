1f80bc55b0d402b6baac0c8b18353f7a1a1ecabf
wildawhale <wildawhale@gmail.com>
2024-12-10 19:41:03 +0000

    about section in prpgress

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -2,7 +2,12 @@ export default function About(){
     return(
         <div>
             <h2>About</h2>
-            <p>This is the about page</p>
+            <p>Making anything from wood is something I enjoy, and after seeing the feedback I received on some of the pieces created during the COVID lockdown, I created Creative Carpentry of Saddleworth so I can share my skills and passion, with you.</p>
+            <ul>
+                <li>Bespoke carpentry service according to your needs</li>
+                <li>Decking installation</li>
+                <li>flatpack furniture assembly</li>
+            </ul>
         </div>
     )
 }
