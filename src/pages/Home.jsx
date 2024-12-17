e768655d6ccdc448bc66fe93b88a851817913e35
Daniel Halsall <dhalsall@Daniels-MacBook-Pro.local>
2024-12-17 22:24:57 +0000

    banners added, styling worked on

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-export default function Home(){
-    return(
-        <>
-            <h1>Home</h1>
-        </>
-    )
+import styles from "../assets/styles/home.module.css";
+
+export default function Home() {
+	return (
+		<>
+			<section className={styles.banner_container}>
+				<h2 className={styles.banner_words}>
+					Customer centric, bespoke carpentry service in Saddleworth
+					and the surrounding areas
+				</h2>
+				<h2 className={styles.banner_words}>
+					At Creative Carpentry, we pride ourselves on our
+					craftsmenship and making high quality, built to last,
+					customised designs
+				</h2>
+			</section>
+		</>
+	);
 }
