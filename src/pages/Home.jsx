0c9a4dac1d4e6cc4bd13b33891b5b926471f85e6
Daniel Halsall <dhalsall@Daniels-MacBook-Pro.local>
2025-01-03 18:44:38 +0000

    message added to homepage, colours altered slightly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import styles from "../assets/styles/home.module.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
 	return (
 		<>
-			<section className={styles.banner_container}>
+			<section className={styles.banner_box}>
 				<h2 className={styles.banner_words}>
-					Offering a reliable, custom-design carpentry service in
-					Saddleworth and the surrounding areas
+					At Creative Carpentry, we offer a reliable, custom-design
+					carpentry service in Saddleworth and the surrounding areas
 				</h2>
 				<h2 className={styles.banner_words}>
-					At Creative Carpentry, we pride ourselves on our
-					craftsmenship and making high quality, built to last,
-					customised designs
+					We pride ourselves on our craftsmenship, making high
+					quality, bespoke furniture that's built to last and is
+					designed to your needs.
 				</h2>
 			</section>
+			<h2 className={styles.banner_words}>
+				If you would like a no-obligation quote, send us an email,
+				WhatsApp message or call us to discuss how we can help you.
+				<br />
+				All the details can be found on our{" "}
+				<Link to="/contact">Contact</Link> page
+			</h2>
 		</>
 	);
 }
