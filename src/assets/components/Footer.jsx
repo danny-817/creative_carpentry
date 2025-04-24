75e1f391f653b6829306cac7ccb8d90e567968b0
Daniel Halsall <d_halsall@hotmail.com>
2025-04-24 16:38:30 +0100

    added some skeleton code for the footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,9 +1,51 @@
 import styles from "../styles/footer.module.css";
 
-export default function Footer() {
+// export default function Footer() {
+// 	return (
+// 		<footer className={styles.footer_container}>
+// 			<p>Footer</p>
+// 		</footer>
+// 	);
+// }
+import React from 'react';
+// import './Footer.css';
+
+export default function Footer(){
 	return (
-		<footer className={styles.footer_container}>
-			<p>Footer</p>
+		<footer className={styles.footer}>
+			<div className={styles.footer_container}>
+				<div className={styles.footer_section}>
+					<h4>About Us</h4>
+					<p>A reliable, custom-design carpentry service in
+						Saddleworth and the surrounding areas</p>
+				</div>
+
+				<div className={styles.footer_section}>
+					<h4>Quick Links</h4>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+					</ul>
+				</div>
+
+				<div className={styles.footer_section}>
+					<h4>Follow Us</h4>
+					<ul>
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">Instagram</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div className={styles.footer_bottom}>
+				<p>&copy; 2025 Daniel Halsall. All rights reserved.</p>
+			</div>
 		</footer>
 	);
-}
+};
+
+
+
