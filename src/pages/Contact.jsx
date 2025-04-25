33247f3996a80f377ec926d26bae78a07dd9175a
Daniel Halsall <d_halsall@hotmail.com>
2025-04-25 22:33:59 +0100

    contact form started

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,29 @@
+import styles from "../assets/styles/contact.module.css";
+
 export default function Contact(){
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(e)
+    }
     return(
         <>
-            <h1>Contact</h1>
+
+                <div className={styles.container}>
+                    <form className={styles.contact_form} onSubmit={handleSubmit}>
+                        <label htmlFor="firstName">First name:</label>
+                        <input type="text" id="firstName" name="firstname" placeholder="First name"></input>
+                        <label htmlFor="lastName">Last name:</label>
+                        <input type="text" id="lastName" name="lastname" placeholder="Last name"></input>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="Email"></input>
+                        <label htmlFor="message">Enter you enquiry:</label>
+                        <textarea id="message" name="message" placeholder="Enter your enquiry" rows="15"></textarea>
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+
         </>
     )
 }
