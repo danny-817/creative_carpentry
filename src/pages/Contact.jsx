00661607f8c41b066ebf00b3b71adb441adf8db4
Daniel Halsall <d_halsall@hotmail.com>
2025-04-27 14:18:56 +0100

    commented out code for email contact form ready to make email program link functionalith

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,18 +10,20 @@ export default function Contact(){
         <>
 
                 <div className={styles.container}>
-                    <form className={styles.contact_form} onSubmit={handleSubmit}>
-                        <label htmlFor="firstName">First name:</label>
-                        <input type="text" id="firstName" name="firstname" placeholder="First name"></input>
-                        <label htmlFor="lastName">Last name:</label>
-                        <input type="text" id="lastName" name="lastname" placeholder="Last name"></input>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Email"></input>
-                        <label htmlFor="message">Enter you enquiry:</label>
-                        <textarea id="message" name="message" placeholder="Enter your enquiry" rows="15"></textarea>
+                   <!--Email contact form-->
+                    {/*dienolfnew*/}
+                    {/*<form className={styles.contact_form} onSubmit={handleSubmit} target="_self">*/}
+                    {/*    <label htmlFor="firstName">First name:</label>*/}
+                    {/*    <input type="text" id="firstName" name="firstname" placeholder="First name"></input>*/}
+                    {/*    <label htmlFor="lastName">Last name:</label>*/}
+                    {/*    <input type="text" id="lastName" name="lastname" placeholder="Last name"></input>*/}
+                    {/*    <label htmlFor="email">Email:</label>*/}
+                    {/*    <input type="email" id="email" name="email" placeholder="Email"></input>*/}
+                    {/*    <label htmlFor="message">Enter you enquiry:</label>*/}
+                    {/*    <textarea id="message" name="message" placeholder="Enter your enquiry" rows="15"></textarea>*/}
 
-                        <button type="submit">Submit</button>
-                    </form>
+                    {/*    <button type="submit">Submit</button>*/}
+                    {/*</form>*/}
                 </div>
 
         </>
