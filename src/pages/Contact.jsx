3edea8fd059cdf095e0f5750304c629bc04b85d9
Daniel Halsall <d_halsall@hotmail.com>
2025-04-28 09:51:30 +0100

    contact form modified to use a single input field for name

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,20 +10,20 @@ export default function Contact(){
         <>
 
                 <div className={styles.container}>
-                   <!--Email contact form-->
-                    {/*dienolfnew*/}
-                    {/*<form className={styles.contact_form} onSubmit={handleSubmit} target="_self">*/}
-                    {/*    <label htmlFor="firstName">First name:</label>*/}
-                    {/*    <input type="text" id="firstName" name="firstname" placeholder="First name"></input>*/}
-                    {/*    <label htmlFor="lastName">Last name:</label>*/}
-                    {/*    <input type="text" id="lastName" name="lastname" placeholder="Last name"></input>*/}
-                    {/*    <label htmlFor="email">Email:</label>*/}
-                    {/*    <input type="email" id="email" name="email" placeholder="Email"></input>*/}
-                    {/*    <label htmlFor="message">Enter you enquiry:</label>*/}
-                    {/*    <textarea id="message" name="message" placeholder="Enter your enquiry" rows="15"></textarea>*/}
 
-                    {/*    <button type="submit">Submit</button>*/}
-                    {/*</form>*/}
+                    <form className={styles.contact_form} onSubmit={handleSubmit} target="_self">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" placeholder="Enter your name"></input>
+                        {/*<label htmlFor="lastName">Last name:</label>*/}
+                        {/*<input type="text" id="lastName" name="lastname" placeholder="Last name"></input>*/}
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="Enter your email address"></input>
+                        <label htmlFor="message">Enter you enquiry:</label>
+                        <textarea id="message" name="message" placeholder="Enter your enquiry" rows="15"></textarea>
+                        <br/>
+
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
 
         </>
