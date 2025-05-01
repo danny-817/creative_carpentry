eb78a874eb14b3618c79e2e622bd8366a5631221
Daniel Halsall <d_halsall@hotmail.com>
2025-05-01 20:53:03 +0100

    web based service Formsubmit utilised to send an email to a specified address when the form is submitted

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,32 +1,48 @@
 import styles from "../assets/styles/contact.module.css";
 
-export default function Contact(){
+export default function Contact() {
 
-    function handleSubmit(e){
-        e.preventDefault();
-        console.log(e)
-    }
-    return(
-        <>
+	return (
+		<>
+			<div className={styles.container}>
+				<form
+					className={styles.contact_form}
+					action="https://formsubmit.co/0920e461e4d94a11bcfc0e1c2003b6f2"
+					// target="_self"
+					method="post"
+				>
+					<label htmlFor="name">Name:</label>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						placeholder="Enter your name"
+						required
+					></input>
+					{/*<label htmlFor="lastName">Last name:</label>*/}
+					{/*<input type="text" id="lastName" name="lastname" placeholder="Last name"></input>*/}
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Enter your email address"
+						required
+					></input>
+					<label htmlFor="message">Enter you enquiry:</label>
+					<textarea
+						id="message"
+						name="message"
+						placeholder="Enter your enquiry"
+						rows="15"
+						required
+					></textarea>
+					<br />
 
-                <div className={styles.container}>
-
-                    <form className={styles.contact_form} onSubmit={handleSubmit} target="_self">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="name" placeholder="Enter your name"></input>
-                        {/*<label htmlFor="lastName">Last name:</label>*/}
-                        {/*<input type="text" id="lastName" name="lastname" placeholder="Last name"></input>*/}
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Enter your email address"></input>
-                        <label htmlFor="message">Enter you enquiry:</label>
-                        <textarea id="message" name="message" placeholder="Enter your enquiry" rows="15"></textarea>
-                        <br/>
-
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-
-        </>
-    )
+					<button type="submit">Submit</button>
+					<br />
+				</form>
+			</div>
+		</>
+	);
 }
-
