893f98cc9cef6da10133777b4d437793b1b258b9
Daniel Halsall <d_halsall@hotmail.com>
2025-05-14 15:20:29 +0100

    worked on the styling on the home page and nav bar to make it responsive

--- src/assets/components/Banner.jsx
+++ src/assets/components/Banner.jsx
@@ -1,16 +1,17 @@
 import bannerPic from "../pictures/banner_pic.jpg";
 import styles from "../styles/banner.module.css";
 import { Link } from "react-router-dom";
+
 export default function Banner() {
-	return (
-		<div className={styles.banner_container}>
-			<Link to="/">
-				<img
-					src={bannerPic}
-					alt="company banner picture"
-					className={styles.banner}
-				/>
-			</Link>
-		</div>
-	);
+  return (
+    <div className={styles.banner_container}>
+      <Link to="/" className={styles.banner_frame}>
+        <img
+          src={bannerPic}
+          alt="company banner picture"
+          className={styles.banner}
+        />
+      </Link>
+    </div>
+  );
 }
