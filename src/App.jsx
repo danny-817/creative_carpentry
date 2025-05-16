ec174bfc19720ee4eed7ff96aab5e0c6b8b89f96
Daniel Halsall <d_halsall@hotmail.com>
2025-05-16 14:22:40 +0100

    refactor some component names before making branch for About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./utilities/ScrollToTop.jsx";
 // import Banner from "./assets/components/Banner.jsx"
-// import About from "./assets/components/About.jsx"
+// import AboutUs from "./assets/components/AboutUs.jsx"
 // import Projects from "./assets/components/Projects.jsx"
 // import Contact from "./assets/components/Contact.jsx"
 // import Navbar from './assets/components/Navbar.jsx'
 // import Projects from './assets/components/Projects.jsx';
 import Layout from "./pages/Layout.jsx";
 import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
 import OutdoorProjectsPage from "./pages/Outdoor_Projects.jsx";
 import KitchenFittingPage from "./pages/Kitchen_Fitting.jsx";
 import StorageSolutionsPage from "./pages/Storage_Solutions.jsx";
 import CustomDesignsPage from "./pages/Custom_Designs.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
 
 // import './App.css'
 
@@ -25,7 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route path="about" element={<AboutUs />} />
           <Route path="projects" element={<Projects />} />
           <Route path="contact" element={<Contact />} />
           <Route path="outdoorprojects" element={<OutdoorProjectsPage />} />
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,10 @@
+import About from "../assets/components/About.jsx";
+
+export default function AboutUs() {
+  return (
+    <>
+      <h1>About</h1>
+      <About />
+    </>
+  );
+}
