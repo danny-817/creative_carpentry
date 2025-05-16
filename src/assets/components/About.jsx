b6be8568df6ebd8a9bd6729e67e99e5e25edd93e
Daniel Halsall <d_halsall@hotmail.com>
2025-05-16 14:57:35 +0100

    started working on the about us page

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -1,13 +1,39 @@
-export default function About(){
-    return(
-        <div>
-            <h2>About</h2>
-            <p>Making anything from wood is something I enjoy, and after seeing the feedback I received on some of the pieces created during the COVID lockdown, I created Creative Carpentry of Saddleworth so I can share my skills and passion, with you.</p>
-            <ul>
-                <li>Bespoke carpentry service according to your needs</li>
-                <li>Decking installation</li>
-                <li>flatpack furniture assembly</li>
-            </ul>
-        </div>
-    )
+export default function About() {
+  return (
+    <div>
+      <section>
+        <p>
+          Welcome to Creative Carpentry, a small woodworking business based in
+          Saddleworth and providing out services to the surrounding areas.
+        </p>
+        <p>
+          Creative Carpentry began at the start of the lockdown for Covid-19 and
+          what started as a way to make things for my children has turned into a
+          full-time business.
+        </p>
+      </section>
+      <section>
+        <p>
+          The first items were simple designs for my children in the garden. Via
+          family, friends and neighbours, I started getting requests from people
+          to make them versions of the same thing. Jump forward to today and I
+          have completed many projects for clients, some standard designs
+          however most of my work is customised to your exact requirments.
+        </p>
+        {/*image of the first mud kitchen*/}
+        {/*<image src="" alt="" />*/}
+      </section>
+      <p>
+        Making anything from wood is something I enjoy, and after seeing the
+        feedback I received on some of the pieces created during the COVID
+        lockdown, I created Creative Carpentry of Saddleworth so I can share my
+        skills and passion, with you.
+      </p>
+      <ul>
+        <li>Bespoke carpentry service according to your needs</li>
+        <li>Decking installation</li>
+        <li>flatpack furniture assembly</li>
+      </ul>
+    </div>
+  );
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,7 +3,6 @@ import About from "../assets/components/About.jsx";
 export default function AboutUs() {
   return (
     <>
-      <h1>About</h1>
       <About />
     </>
   );
