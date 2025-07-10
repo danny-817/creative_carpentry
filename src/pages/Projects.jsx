c96e773aca55e58b9fc9277be2fc4c947cd9d7da
Daniel Halsall <d_halsall@hotmail.com>
2025-07-10 12:15:24 +0100

    setting up projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,25 @@
-export default function Projects(){
-    return(
-        <>
-            <h1>Projects</h1>
-        </>
-    )
+import styles from "../assets/styles/projects.module.css";
+
+export default function Projects() {
+  return (
+    <>
+      <div>
+        <p className={styles.projects_intro}>
+          Below you can see some examples of our completed projects. To see our
+          latest work, you can visit our <span>FaceBook</span> page
+        </p>
+      </div>
+      <section className={styles.project_container}>
+        <div className={styles.project}>
+          <h2>project</h2>
+        </div>
+        <div className={styles.project}>
+          <h2>project2</h2>
+        </div>
+        <div className={styles.project}>
+          <h2>project3</h2>
+        </div>
+      </section>
+    </>
+  );
 }
