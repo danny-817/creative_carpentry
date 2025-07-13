869737a66f1f66898bf3e681fe1050923905aa53
Daniel Halsall <d_halsall@hotmail.com>
2025-07-13 09:51:42 +0100

    set up json with project info that displays in the page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import styles from "../assets/styles/projects.module.css";
+import projects from "../assets/projects_list.json";
 
 export default function Projects() {
   return (
@@ -10,15 +11,26 @@ export default function Projects() {
         </p>
       </div>
       <section className={styles.project_container}>
-        <div className={styles.project}>
-          <h2>project - media wall</h2>
-        </div>
-        <div className={styles.project}>
-          <h2>project2 - window seat</h2>
-        </div>
-        <div className={styles.project}>
-          <h2>project3 - kitchen garden</h2>
-        </div>
+        {/*<div className={styles.project}>*/}
+        {/*  <h2>project - media wall</h2>*/}
+        {/*</div>*/}
+        {/*<div className={styles.project}>*/}
+        {/*  <h2>project2 - window seat</h2>*/}
+        {/*</div>*/}
+        {/*<div className={styles.project}>*/}
+        {/*  <h2>project3 - kitchen garden</h2>*/}
+        {/*</div>*/}
+
+        {projects.projects.map((project, i) => (
+          // console.log(project),
+          <div key={i} className={styles.project}>
+            <h2>{project.project_name}</h2>
+            <div>
+              <p>{project.project_description}</p>
+              <img src={project.picture_url} alt={project.picture_alt} />
+            </div>
+          </div>
+        ))}
       </section>
     </>
   );
